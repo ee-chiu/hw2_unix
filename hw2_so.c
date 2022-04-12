1ffff9cd74b713a42332dc15b2943c2802abd461
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2022-04-13 00:16:30 +0800

    chmod done + fix creat

--- hw2_so.c
+++ hw2_so.c
@@ -2,15 +2,28 @@
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-void creat(const char* path, mode_t mode){
-    char* real_path;
+int chmod(const char* path, mode_t mode){
+    char* real_path = calloc(256, sizeof(char));
+    realpath(path, real_path);
+    int (*old_chmod)(const char*, mode_t) = NULL;
+    void* handle = dlopen("libc.so.6", RTLD_LAZY);
+    if(!handle) { printf("chmod handle error!\n"); return -1; }
+    old_chmod = dlsym(handle, "chmod");
+    if(!old_chmod) { printf("old_chmod error!\n"); return -1; }
+    printf("[logger] chmod(\"%s\", %o) = %d\n", real_path, mode, old_chmod(real_path, mode));
+    return old_chmod(path, mode);
+}
+
+int creat(const char* path, mode_t mode){
+    char* real_path = calloc(256, sizeof(char));
     realpath(path, real_path);
     int (*old_creat)(const char*, mode_t) = NULL;
     void* handle = dlopen("libc.so.6", RTLD_LAZY);
-    if(!handle) { printf("creat handle error!\n"); return; }
+    if(!handle) { printf("creat handle error!\n"); return -1; }
     old_creat = dlsym(handle, "creat");
-    if(!old_creat) { printf("old_creat error!\n"); return; }
-    printf("[logger] creat(\"%s\", %u) = %d\n", path, mode, old_creat(path, mode));
-    return;
+    if(!old_creat) { printf("old_creat error!\n"); return -1; }
+    printf("[logger] creat(\"%s\", %o) = %d\n", real_path, mode, old_creat(real_path, mode));
+    return old_creat(real_path, mode);
 }
